a2c40a7ad822c2c6d572795a305df707d4e62e4d
Jimi M <jimi.maiquez@gmail.com>
2020-01-08 21:16:23 +0100

    Use RSS feed for Hacker news

--- lib/hackernewscard.dart
+++ lib/hackernewscard.dart
@@ -1,8 +1,7 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:launcher/feedcard.dart';
 import 'package:http/http.dart' as http;
+import 'package:webfeed/webfeed.dart';
 
 class HackerNewsCard extends StatefulWidget {
   HackerNewsCard({Key key}) : super(key: key);
@@ -15,35 +14,35 @@ class _HackerNewsCardState extends State<HackerNewsCard> {
   @override
   void initState() {
     super.initState();
-    fetchTopStories();
   }
   
-  Future<dynamic> fetchTopStories() async {
-    var response = await http.get("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty&orderBy=\"\$key\"&limitToFirst=30");
+  Future<List<RssItem>> fetchTopStories() async {
+    var response = await http.get("https://news.ycombinator.com/rss");
 
-    var stories = json.decode(response.body);
-    stories = stories.map((storyId) async {
-      var response = await http.get("https://hacker-news.firebaseio.com/v0/item/$storyId.json?print=pretty");
-      return json.decode(response.body);
-    }).toList();
-    print(stories);
+    var feed = new RssFeed.parse(response.body);
+    return feed.items;
   }
 
   @override
   Widget build(BuildContext context) {
     return FeedCard(
       "Hacker news",
-      Column(
-        children: <Widget>[
-          Container(
-            child: Text(
-              'Lorem ipsum',
-              style: TextStyle(
-                color: Colors.white
-              )
-            ),
-          )
-        ],
+      FutureBuilder<List<RssItem>>(
+        future: fetchTopStories(),
+        builder: (BuildContext context, AsyncSnapshot<List<RssItem>> snapshot) {
+          return Column(
+            children: snapshot.data.map((item) => Container(
+              margin: EdgeInsets.only(bottom: 10),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                item.title,
+                style: TextStyle(
+                  color: Colors.white
+                ),
+              ),
+            )).toList()
+          );
+        },
       )
     );
   }
